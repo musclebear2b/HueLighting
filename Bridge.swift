3b50c2fd0fbbb5a9275a84dea7f9dfe6240a55aa
Richard Whent <richard@chunkster.net>
2017-01-02 04:13:48 +0000

    Creation of Bridge Class

---
+++ Bridge.swift
@@ -0,0 +1,13 @@
+//
+//  Bridge.swift
+//  HueLighting
+//
+//  Created by Richard Whent on 02/01/2017.
+//  Copyright © 2017 Chunkster.net. All rights reserved.
+//
+
+import UIKit
+
+class Bridge: NSObject {
+
+}
