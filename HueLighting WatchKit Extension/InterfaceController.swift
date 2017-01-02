2a687db197183dae4908524c70119ef0606446b0
Richard Whent <richard@chunkster.net>
2017-01-02 03:39:49 +0000

    Initial Commit

---
+++ HueLighting WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HueLighting WatchKit Extension
+//
+//  Created by Richard Whent on 02/01/2017.
+//  Copyright © 2017 Chunkster.net. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
