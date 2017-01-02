7f110852f460cab2ce7fe561037ca30738f47eba
Richard Whent <richard@chunkster.net>
2017-01-02 03:47:43 +0000

    Initial Creation
    Created base files and added frameworks.
    Created WorkingBranch branch

---
+++ HueLighting/HueLighting-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  HueLighting-Bridging-Header.h
+//  HueLighting
+//
+//  Created by Richard Whent on 02/01/2017.
+//  Copyright © 2017 Chunkster.net. All rights reserved.
+//
+
+#ifndef HueLighting_Bridging_Header_h
+#define HueLighting_Bridging_Header_h
+
+
+#endif /* HueLighting_Bridging_Header_h */
+#import <HueSDK_iOS/HueSDK.h>
---
+++ dummy.h
@@ -0,0 +1,13 @@
+//
+//  dummy.h
+//  HueLighting
+//
+//  Created by Richard Whent on 02/01/2017.
+//  Copyright © 2017 Chunkster.net. All rights reserved.
+//
+
+#ifndef dummy_h
+#define dummy_h
+
+
+#endif /* dummy_h */
